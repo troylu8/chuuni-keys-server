91083d7db4ae1bc1c4afa97b1eefb1596ea6de48
troy <troylu8@gmail.com>
2025-07-09 16:58:53 -0700

    use owner_hash

--- server.py
+++ server.py
@@ -13,19 +13,17 @@ import shutil
 app = Flask(__name__)
 CORS(app, origins="*")
 
-PAGE_SIZE = 50
 
-
-def gen_rand_str(n: int = 10):
+def gen_id():
     symbols = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-_"
-    return "".join([random.choice(symbols) for _ in range(n)])
+    return "".join([random.choice(symbols) for _ in range(10)])
 
 
-def create_db_row(online_id: str, metadata: dict, ownership_key: bytes):
+def create_db_row(id: str, metadata: dict, owner_hash: str):
     """ returns `( query placeholder, row data )` """
     
     data = (
-        online_id,
+        id,
         metadata["title"],
         metadata["difficulty"],
         metadata["bpm"],
@@ -38,7 +36,7 @@ def create_db_row(online_id: str, metadata: dict, ownership_key: bytes):
         metadata.get("credit_audio"),
         metadata.get("credit_img"),
         metadata.get("credit_chart"),
-        bcrypt.hashpw(ownership_key, bcrypt.gensalt())
+        owner_hash
     )
     return ( ','.join( ['?'] * len(data) ) , data)
 
@@ -74,6 +72,10 @@ def save_uploaded_files(id: str, exts: dict[str, str]):
     if "img" in request.files and exts["img_ext"] in VALID_IMG_EXTS:
         request.files["img"].save(f"{chart_folder}/img.{exts["img_ext"]}")
 
+
+PAGE_SIZE = 50
+
+
 @app.get("/charts/<int:page>")
 def get_charts(page: int):
     with sqlite3.connect("data/chuuni.db") as conn:
@@ -92,38 +94,37 @@ def get_charts(page: int):
 def upload_chart():
     
     metadata = json.load(request.files["metadata"].stream)
+    owner_hash = request.form["owner_hash"];
     
-    # generate online_id and ownership_key
-    online_id = gen_rand_str()
-    ownership_key = random.randbytes(32)
+    online_id = gen_id()
     
     save_uploaded_files(online_id, metadata)
     
     # insert sqlite3 row
-    placeholder, row_data = create_db_row(online_id, metadata, ownership_key)
+    placeholder, row_data = create_db_row(online_id, metadata, owner_hash)
     with sqlite3.connect("data/chuuni.db") as conn:
         conn.cursor().execute(f"INSERT INTO charts VALUES ({placeholder})", row_data)
     
-    return [online_id, ownership_key.hex()]
+    return online_id
 
 
 @app.patch("/charts/<id>")
 def update_chart(id: str):
-    ownership_key = bytes.fromhex(request.form["ownership_key"])
+    owner_key = request.form["owner_key"].encode();
     
     # update sqlite3 row
     with sqlite3.connect("data/chuuni.db") as conn:
         cursor = conn.cursor()
         
-        old_data = cursor.execute("SELECT img_ext, ownership_key FROM charts WHERE id=?", (id,)).fetchone()
+        old_data = cursor.execute("SELECT img_ext, owner_hash FROM charts WHERE id=?", (id,)).fetchone()
         if old_data is None:
             return Response(status=404)
         
-        old_img_ext, hash = old_data
+        old_img_ext, owner_hash = old_data
         
-        if bcrypt.checkpw(ownership_key, hash):
+        if bcrypt.checkpw(owner_key, owner_hash.encode()):
             metadata: dict = json.load(request.files["metadata"].stream)
-            placeholder, new_row_data = create_db_row(id, metadata, ownership_key)
+            placeholder, new_row_data = create_db_row(id, metadata, owner_hash)
             cursor.execute(f"REPLACE INTO charts VALUES ({placeholder})", new_row_data)
         else:
             return Response(status=401)
@@ -139,17 +140,17 @@ def update_chart(id: str):
 
 @app.delete("/charts/<id>")
 def delete_chart(id: str):
-    ownership_key = request.data
+    owner_key = request.data
     
     # delete sqlite3 row
     with sqlite3.connect("data/chuuni.db") as conn:
         cursor = conn.cursor()
         
-        row = cursor.execute("SELECT ownership_key FROM charts WHERE id=?", (id,)).fetchone()
+        row = cursor.execute("SELECT owner_hash FROM charts WHERE id=?", (id,)).fetchone()
         if row is None:
             return Response(status=404)
         
-        if bcrypt.checkpw(ownership_key, row[0]):
+        if bcrypt.checkpw(owner_key, row[0].encode()):
             cursor.execute("DELETE FROM charts WHERE id=?", (id,))
         else:
             return Response(status=401)
@@ -165,16 +166,14 @@ def download_chart(id: str):
     # get metadata from sqlite3 row
     with sqlite3.connect("data/chuuni.db") as conn:
         conn.row_factory = sqlite3.Row
-        cursor = conn.cursor()
         
-        row = cursor.execute("SELECT * FROM charts WHERE id=?", (id,)).fetchone()
+        row = conn.cursor().execute("SELECT * FROM charts WHERE id=?", (id,)).fetchone()
         if row is None:
             return Response(status=404)
     
     # zip chart folder
     zip_buffer = io.BytesIO()
     chart_folder = f"data/charts/{id}"
-    
     with ZipFile(zip_buffer, mode="w") as zip:
         zip.writestr("metadata.json", json.dumps(to_metadata(row)))
         zip.write(f"{chart_folder}/chart.txt", "chart.txt")
